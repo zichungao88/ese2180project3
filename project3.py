608b77e0aa955c6038183edecb7cfddad83b2007
Zichun (Jerry) Gao <zichungao88@gmail.com>
2024-11-28 00:56:32 -0500

    Setup main project file

---
+++ project3.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import imageio
+
+# 1 TODO: Download dataset & load training dataset
+
+
+
+
+# 2 TODO: Generate matrix X
+
+
+
+
+# 3 TODO: Compute SVD of X & plot singular values
+
+
+
+
+# 4 TODO: Limit # of features & plot og image w/ aforementioned features
+
+
+
+
+# 5 TODO: Test dataset
+
+
+
+
+# 6 TODO: Repeat test w/ rotated image
